c67c2b21383f98d3c6a7c02445f3fbc7fcc84d1c
TanMandeep <mandeeptaneja21@outlook.com>
2017-12-16 22:52:04 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,13 +15,48 @@
 
 FORCE_32_BIT := true
 
-include device/cyanogen/msm8916-common/BoardConfigCommon.mk
-
-DEVICE_PATH := device/wingtech/wt88047
+#include device/cyanogen/msm8916-common/BoardConfigCommon.mk
+DEVICE_PATH := device/xiaomi/gucci
+#DEVICE_PATH := device/wingtech/wt88047
 include $(DEVICE_PATH)/board/*.mk
 
 # inherit from the proprietary version
-include vendor/wingtech/wt88047/BoardConfigVendor.mk
+include vendor/xiaomi/gucci/BoardConfigVendor.mk
 
-#vendorsetup.sh
-include device/wingtech/wt88047/vendorsetup.sh
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/xiaomi/gucci/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := gucci
+
+BOARD_KERNEL_CMDLINE := 
+BOARD_KERNEL_BASE := 0x
+BOARD_KERNEL_PAGESIZE := 
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_KERNEL_SOURCE := kernel/xiaomi/gucci
+
+# build old-style zip files (required for ota updater)
+BLOCK_BASED_OTA := false
+
+# system.prop
+#TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
+BOARD_HAS_NO_SELECT_BUTTON := true
