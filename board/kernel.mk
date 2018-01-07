660332ff6346634fd05a3f0302c124d486902a67
Immortal Grenadiers <mandeeptaneja21@outlook.com>
2018-01-07 19:46:05 +0530

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,8 @@
 # Kernel
 BOARD_DTBTOOL_ARGS := -2
-BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_SEPARATED_DT := false
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
 TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
 BOARD_KERNEL_CMDLINE += sched_enable_hmp=1 phy-msm-usb.floated_charger_enable=1
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
