4c33536a9fe26de9e8c192c399a0dee3d63c4b7f
Immortal Grenadiers <mandeeptaneja21@outlook.com>
2018-01-07 19:47:38 +0530

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,6 +3,6 @@ BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_SEPARATED_DT := false
 TARGET_KERNEL_SOURCE := kernel/wingtech/msm8916
-TARGET_KERNEL_CONFIG := lineageos_wt88047_defconfig
+TARGET_KERNEL_CONFIG := gucci_defconfig
 BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 phy-msm-usb.floated_charger_enable=1
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
